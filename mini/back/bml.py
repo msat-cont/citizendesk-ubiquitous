10492817583766b160a143387f87d3126c39e280
Martin Saturka <martin.saturka@sourcefabric.org>
2013-11-04 18:30:48 +0100

    Images support.

--- mini/back/bml.py
+++ mini/back/bml.py
@@ -27,6 +27,21 @@ def take_bml():
 
         return (tag_string, 200, {'Content-Type': 'application/javascript'})
 
+    snippets = []
+
+    session_value = None
+    session_param = 'session'
+    if session_param in request.args:
+        session_value = str(request.args[session_param])
+
+        if session_value:
+            cursor = mongo.db.snippets.find({'session': session_value}).sort([('created', -1)])
+            for entry in cursor:
+                entry['_id'] = str(entry['_id'])
+                entry['created'] = entry['created'].isoformat() if not None else None
+                snippets.append(entry)
+            return (json.dumps(snippets), 200, {'Content-Type': 'application/json'})
+
     got_ref = None
     ref_param = 'user'
     if ref_param in request.args:
@@ -35,11 +50,11 @@ def take_bml():
     if got_ref is None:
         return '<html><body><div style="display:none">placeholder</div></body></html>'
 
-    snippets = []
     if '*' == got_ref:
         cursor = mongo.db.snippets.find()
     else:
         cursor = mongo.db.snippets.find({'user': got_ref})
+    cursor.sort([('created', -1)])
     for entry in cursor:
         entry['_id'] = str(entry['_id'])
         entry['created'] = entry['created'].isoformat() if not None else None
@@ -53,7 +68,6 @@ def save_bml():
     mongo = mongo_dbs[minicd_inner]
 
     snippet = {}
-    snippet['selected'] = None;
     snippet['user'] = None;
     snippet['session'] = None;
     snippet['provider'] = None;
@@ -66,6 +80,22 @@ def save_bml():
     for part in snippet:
         if snippet[part] is None:
             return 'not all data provided', 404
+
+    snippet_payload = {}
+    snippet_payload['text_snippet'] = None;
+    snippet_payload['image_title'] = None;
+    snippet_payload['image_url'] = None;
+    snippet_payload['image_png'] = None;
+
+    got_payload = False
+    for part in snippet_payload:
+        if part in request.form:
+            snippet_payload[part] = str(request.form[part].encode('utf8'))
+            got_payload = True
+        snippet[part] = snippet_payload[part]
+
+    if not got_payload:
+        return 'no payload provided', 404
 
     snippet_id = mongo.db.snippets.insert(snippet)
 
--- mini/front/lets/mini_cd.js
+++ mini/front/lets/mini_cd.js
@@ -96,6 +96,15 @@
 
 
     function make_form(base_url) {
+        var menu = document.createElement('div');
+        menu.setAttribute('id', 'mini_cd_txt_menu');
+        $('#mini_cd_txt_menu').css('display', 'none');
+        $('#mini_cd_txt_menu').html('<div>&nbsp;</div>');
+
+        window.mini_cd_save_text_snippet = function() {
+            $('#mini_cd_form').submit();
+        };
+
         var iframe = document.createElement('iframe');
         iframe.setAttribute('name', 'frame_cd_post');
         iframe.setAttribute('src', base_url);
@@ -109,7 +118,10 @@
         form.setAttribute('target', 'frame_cd_post');
 
         var params = {
-            'selected': '' + get_sel_text(),
+            'text_snippet': '' + get_sel_text(),
+            'image_title': '',
+            'image_url': '',
+            'image_png': '',
             'user': '' + window.cd_user_name,
             'session': '' + window.cd_session_id,
             'provider': '' + window.location.href
@@ -123,22 +135,65 @@
             form.appendChild(oneField);
         }
 
-        var submitField = document.createElement('input');
-        submitField.setAttribute('type', 'submit');
-        submitField.setAttribute('value', 'send selection');
-        form.appendChild(submitField);
+        $(form).css('display', 'none');
 
-        $(form).css('position', 'fixed');
-        $(form).css('top', '10px');
-        $(form).css('left', '10px');
-        $(form).css('z-index', '10000');
+        window.mini_cd_iframe_status = 0;
+        $(iframe).on('load', function(ev) {
+            window.mini_cd_iframe_status += 1;
+            // for Chromium under Linux
+            if (navigator.userAgent.toLowerCase().indexOf('webkit') > -1) {
+                if (3 < window.mini_cd_iframe_status) {
+                    if (1 == (window.mini_cd_iframe_status % 2)) {
+                        window.history.back();
+                        return;
+                    }
+                }
+            }
+            // for Firefox under Linux
+            if (2 == window.mini_cd_iframe_status) {
+                window.history.back();
+            }
+        });
 
         $(form).on('submit', function(ev) {
-            $(form).find('input[name="selected"]').val('' + get_sel_text());
         });
 
         document.body.appendChild(iframe);
         document.body.appendChild(form);
+
+        $('body').not('img').on('contextmenu', function(e) {
+            var cur_snippet = '' + get_sel_text();
+            if ('' == cur_snippet) {
+                return true;
+            }
+            $('#mini_cd_form').find('input[name="text_snippet"]').val('' + get_sel_text());
+
+            var menu_html = '<div style="cursor:pointer" onClick="window.mini_cd_save_text_snippet(); return false;">Save text snippet</div>';
+
+            $('#mini_cd_txt_menu').html(menu_html);
+
+            $('#mini_cd_txt_menu').css({
+                'display': 'none',
+                'position': 'absolute',
+                'padding': '10px',
+                'background-color': '#ddd',
+                'border': '1px solid #000',
+                'z-index': 10000
+            });
+
+            $('#mini_cd_txt_menu').css('top', e.pageY+'px');
+            $('#mini_cd_txt_menu').css('left', e.pageX+'px');
+            $('#mini_cd_txt_menu').show();
+
+            return false;
+        });
+
+        $(document).click(function() {
+            $('#mini_cd_txt_menu').hide();
+        });
+
+        document.body.appendChild(menu);
+
     };
 
     function prepare_open(base_url) {
@@ -150,17 +205,20 @@
         form.setAttribute('target', '_blank');
 
         var submitField = document.createElement('input');
+        submitField.setAttribute('id', 'mini_cd_page_session');
         submitField.setAttribute('type', 'submit');
-        submitField.setAttribute('value', 'commit data');
+        submitField.setAttribute('value', 'page session');
         form.appendChild(submitField);
 
+        $(submitField).css('width', '120px');
+
         $(form).css('position', 'fixed');
-        $(form).css('top', '40px');
+        $(form).css('top', '10px');
         $(form).css('left', '10px');
         $(form).css('z-index', '10000');
 
         $(form).on('submit', function(ev) {
-            var commit_win = window.open(base_url + '?user=' + window.cd_user_name, 'commit_win');
+            var commit_win = window.open(base_url + '?session=' + window.cd_session_id, 'commit_win');
             window.commit_win_open = true;
             return false;
         });
@@ -175,12 +233,15 @@
         form.setAttribute('target', '_blank');
 
         var submitField = document.createElement('input');
+        submitField.setAttribute('id', 'mini_cd_find_terms');
         submitField.setAttribute('type', 'submit');
         submitField.setAttribute('value', 'find terms');
         form.appendChild(submitField);
 
+        $(submitField).css('width', '120px');
+
         $(form).css('position', 'fixed');
-        $(form).css('top', '70px');
+        $(form).css('top', '40px');
         $(form).css('left', '10px');
         $(form).css('z-index', '10000');
 
@@ -196,6 +257,102 @@
         document.body.appendChild(form);
     };
 
+    function mini_cd_save_image_url_inner(img_url, img_title) {
+        $('#mini_cd_form').find('input[name="image_url"]').val('' + img_url);
+        $('#mini_cd_form').find('input[name="image_title"]').val('' + img_title);
+        $('#mini_cd_form').submit();
+    };
+
+    function mini_cd_save_image_bitmap_inner(img_bitmap, img_url, img_title) {
+        $('#mini_cd_form').find('input[name="image_png"]').val('' + img_bitmap);
+        $('#mini_cd_form').find('input[name="image_url"]').val('' + img_url);
+        $('#mini_cd_form').find('input[name="image_title"]').val('' + img_title);
+        $('#mini_cd_form').submit();
+    };
+
+    function prepare_images() {
+
+        var image_url_string = null;
+        var image_bitmap_string = null;
+        var image_title = null;
+
+        window.mini_cd_save_image_url = function() {
+            mini_cd_save_image_url_inner(image_url_string, image_title);
+        }
+        window.mini_cd_save_image_bitmap = function() {
+            mini_cd_save_image_bitmap_inner(image_bitmap_string, image_url_string, image_title);
+        }
+
+        var menu = document.createElement('div');
+        menu.setAttribute('id', 'mini_cd_img_menu');
+        $(menu).css('display', 'none');
+        $(menu).html('<div>&nbsp;</div>');
+
+        $('img').bind('contextmenu', function(e) {
+            $('#mini_cd_form').find('input[name="text_snippet"]').val('' + get_sel_text());
+
+            var event_img = e.currentTarget;
+
+            image_title = event_img.title;
+            image_url_string = event_img.src;
+
+            var doc_host = document.location.host;
+            var doc_port = document.location.port;
+            var doc_protocol = document.location.protocol;
+
+            var tmp_href = document.createElement('a');
+            tmp_href.href = image_url_string;
+
+            var host_same = (tmp_href.host == document.location.host);
+            var port_same = (tmp_href.port == document.location.port);
+
+            var has_png = false;
+
+            if (host_same && port_same) {
+                try {
+                    var tmp_canvas = document.createElement('canvas');
+                    var tmp_context = tmp_canvas.getContext('2d');
+                    tmp_context.drawImage(event_img, 0, 0);
+                    image_bitmap_string = tmp_canvas.toDataURL('image/png');
+
+                    has_png = true;
+
+                } catch(exc) {
+                    has_png = false;
+                }
+
+            }
+
+            var menu_html = '<div style="cursor:pointer" onClick="window.mini_cd_save_image_url(); return false;">Save image URL</div>';
+            if (has_png) {
+                menu_html += '<div style="cursor:pointer; margin-top: 4px" onClick="window.mini_cd_save_image_bitmap(); return false;">Save image bitmap</div>';;
+            }
+
+            $('#mini_cd_img_menu').html(menu_html);
+
+            $('#mini_cd_img_menu').css({
+                'display': 'none',
+                'position': 'absolute',
+                'padding': '10px',
+                'background-color': '#ddd',
+                'border': '1px solid #000',
+                'z-index': 10000
+            });
+
+            $('#mini_cd_img_menu').css('top', e.pageY+'px');
+            $('#mini_cd_img_menu').css('left', e.pageX+'px');
+            $('#mini_cd_img_menu').show();
+
+            return false;
+        });
+
+        $(document).click(function() {
+            $('#mini_cd_img_menu').hide();
+        });
+
+        document.body.appendChild(menu);
+    };
+
     function init_bookmarklet() {
         load_tags(window.cd_feed_url, window.cd_feed_name);
     };
@@ -203,6 +360,8 @@
     function setup_bookmarklet() {
         (window.cd_bookmarklet = function() {
             if (!window.cd_is_initialized) {
+                prepare_images();
+
                 window.cd_session_id = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
                 make_form(window.cd_feed_url);
                 prepare_open(window.cd_feed_url);
